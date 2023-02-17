03edac9dbe981efcb9c84ac9dd98539f524d38d5
Treasure-B <thabiso.magagula@gmail.com>
2023-02-16 17:48:29 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,8 @@ int main(void)
 {
 printf("The size of char is %ld bytes.\n", sizeof(char));
 printf("The size of int is %ld bytes.\n", sizeof(int));
-printf("The size of long int is %ld bytes.\n", sizeof(long));
-printf("The size of long long int is %ld bytes.\n", sizeof(long long));
+printf("The size of long int is %ld bytes.\n", sizeof(long int));
+printf("The size of long long int is %ld bytes.\n", sizeof(long long int));
 printf("The size of float is %ld bytes.\n", sizeof(float));
 return (0);
 }
