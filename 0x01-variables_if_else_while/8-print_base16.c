a75e23f080e9fd3f394760dd713c4ed8e05785b2
Treasure-B <thabiso.magagula@gmail.com>
2023-02-17 18:28:33 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+/**
+ * main - prints all the numbers of base 16 in lowercase, \n
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	char cbet;
+
+	for (cbet = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (cbet = 'a'; cbeth <= 'f'; cbet++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
+
