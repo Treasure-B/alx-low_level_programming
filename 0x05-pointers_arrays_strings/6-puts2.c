9084957cff7ed58e3d46e6cc3e0427214e79201a
Treasure-B <thabiso.magagula@gmail.com>
2023-02-27 18:06:39 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,16 +11,24 @@
 
 void puts2(char *str)
 {
-int i = 0;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-while (str[i] != '\0')
-{
-if (i % 2 == 0)
-putchar(str[i]);
-i++;
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putchar('\n');
 }
-putchar('\n');
-}
-
-
 
