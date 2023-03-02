03b294e87578861f22e3385b9327d9dfde430bf3
Treasure-B <thabiso.magagula@gmail.com>
2023-03-02 08:02:24 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,28 +8,32 @@
 
 char *cap_string(char *str)
 {
-if (str == NULL)
-{
-return (NULL);
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+
+		if (str[i - 1] == ' ' ||
+		    str[i - 1] == '\t' ||
+		    str[i - 1] == '\n' ||
+		    str[i - 1] == ',' ||
+		    str[i - 1] == ';' ||
+		    str[i - 1] == '.' ||
+		    str[i - 1] == '!' ||
+		    str[i - 1] == '?' ||
+		    str[i - 1] == '"' ||
+		    str[i - 1] == '(' ||
+		    str[i - 1] == ')' ||
+		    str[i - 1] == '{' ||
+		    str[i - 1] == '}' ||
+		    i == 0)
+			str[i] -= 32;
+
+		i++;
+	}
+
+	return (str);
 }
 
-for (int i = 0; str[i] != '\0'; i++)
-{
-if (islower(str[i]) && (i == 0 || isspace(str[i - 1]) ||
-str[i - 1] == ',' ||
-str[i - 1] == ';' ||
-str[i - 1] == '.' ||
-str[i - 1] == '!' ||
-str[i - 1] == '?' ||
-str[i - 1] == '"' ||
-str[i - 1] == '(' ||
-str[i - 1] == ')' ||
-str[i - 1] == '{' ||
-str[i - 1] == '}'))
-{
-str[i] = toupper(str[i]);
-}
-}
-
-return (str);
-}
