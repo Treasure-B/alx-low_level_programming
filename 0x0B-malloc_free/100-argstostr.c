e5d27e398cfb90120cd19cacb1a6885b30fefe47
Treasure-B <thabiso.magagula@gmail.com>
2023-03-15 15:09:25 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,47 +2,48 @@
 #include <stdlib.h>
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: int input
- * @av: double pointer array
- * Return: 0
- */
+* argstostr - concatenates all the arguments of your program
+* @ac: int input
+* @av: double pointer array
+* Return: 0
+*/
 
 char *argstostr(int ac, char **av)
 {
-	int i, j, m = 0, l = 0;
-	char *str;
+int i, j, len = 0, index = 0;
+char *str;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+if (ac == 0 || av == NULL)
+return (NULL);
 
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			i++;
-	}
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+len++;
+}
+len++;
+}
+str = malloc(sizeof(char) * len);
+if (str == NULL)
+return (NULL);
 
-	l += ac;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j]; j++)
+{
+str[index] = av[i][j];
+index++;
+}
+if (i != ac - 1)
+{
+str[index] = '\n';
+index++;
+}
+}
 
-	str = malloc(sizeof(char) * l + 1);
+str[index] = '\0';
 
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-		{
-			str[m] = av[i][j];
-			m++;
-		}
-
-		if (str[m] == '\0')
-		{
-			str[m++] = '\n';
-		}
-	}
-
-		return (str);
+return (str);
 }
 
