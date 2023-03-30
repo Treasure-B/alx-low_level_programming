2c114d85b042bb21068d9bb1fc10485c558f4f0b
Treasure-B <thabiso.magagula@gmail.com>
2023-03-30 13:47:19 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -8,7 +9,6 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 
 typedef struct list_s
