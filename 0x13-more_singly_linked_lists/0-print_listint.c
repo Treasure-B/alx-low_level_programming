557ee7c8081883a8fc4cc2dd1d4a3f5a4e746dc7
Treasure-B <thabiso.magagula@gmail.com>
2023-04-03 20:13:12 -0700

    Print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements
+ * @h: prints linked lists
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	size_t n_count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		n_count++;
+		h = h->next;
+	}
+
+	return (n_count);
+}
