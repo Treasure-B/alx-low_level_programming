9b62bc3d2c884f966dea9f6cd0f10e2c27b78183
Treasure-B <thabiso.magagula@gmail.com>
2023-08-21 15:16:25 -0700

    library dynamic

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: starting address to be filled
+ * @b: desired value
+ * @n: number of bytes to be changed
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+
+}
---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strlen -  returns the length of a string
+ * @s: string to get the length
+ * Return: length of @s
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+while (*s != '\0')
+{
+len++;
+s++;
+}
+return (len);
+}
+
